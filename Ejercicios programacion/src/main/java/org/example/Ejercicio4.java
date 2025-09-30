d25476b94ffb16dba5fca124d2ac825b3a5abe46
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-09-30 21:10:39 +0200

    Ejercicio 4

---
+++ Ejercicios programacion/src/main/java/org/example/Ejercicio4.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    static void main(){
+        Scanner teclado = Scanner new teclado.nextInt(System.in);
+
+    }
+}
