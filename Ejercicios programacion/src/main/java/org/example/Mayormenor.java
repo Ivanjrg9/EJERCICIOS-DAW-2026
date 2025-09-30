d769c5c17b9733f4cd5aee842889023928a3631a
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-09-30 20:15:08 +0200

    ejercicio3

--- Ejercicios programacion/src/main/java/org/example/Mayormenor.java
+++ Ejercicios programacion/src/main/java/org/example/Mayormenor.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class Mayormenor {
     static void main() {
-
-        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+             Scanner teclado = new Scanner(System.in);
 
         int numero = teclado.nextInt();
 
         if (numero > 0){
-            System.out.println("Elnumero" + numero + " Es POSITIVO");
+            System.out.println("El numero " + numero + " Es POSITIVO");
 
         }else{
             if (numero<0) {
-                System.out.println("El numero" + numero + "Es NEGATIVO");
+                System.out.println("El numero " + numero + " es NEGATIVO");
             }else {
+                System.out.println("El numero " + numero + " es NATURAL");
 
             }
 
 
 
         }
-            System.out.println();
+
 
 }
     }
