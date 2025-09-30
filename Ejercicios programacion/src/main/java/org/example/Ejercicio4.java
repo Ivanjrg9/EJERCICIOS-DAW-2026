abd3a58cc8dcd878bb4cbe0599ccf933d64f59d5
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-09-30 21:18:20 +0200

    ejercicio 4 continuacion

--- Ejercicios programacion/src/main/java/org/example/Ejercicio4.java
+++ Ejercicios programacion/src/main/java/org/example/Ejercicio4.java
@@ -6,5 +6,18 @@ public class Ejercicio4 {
     static void main(){
         Scanner teclado = Scanner new teclado.nextInt(System.in);
 
+        System.out.println(" Introduce un numero ");
+        int numero1 =teclado.nextInt();
+
+        System.out.println(" Introduce otro numero ");
+        int numero2 = teclado.nextInt();
+
+        System.out.println( " Introduce un ultimo numero" );
+        int numero3 = teclado.nextInt():
+
+        if (numero1 = )
+
+
+
     }
 }
