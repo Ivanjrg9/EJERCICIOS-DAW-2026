bff25d90bd2c84d44b4ddde5ef01cd840f7f2ad0
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-09-30 20:06:45 +0200

    Ejercicio 3 mayor menor

---
+++ Ejercicios programacion/src/main/java/org/example/Mayormenor.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Mayormenor {
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int numero = teclado.nextInt();
+
+        if (numero > 0){
+            System.out.println("Elnumero" + numero + " Es POSITIVO");
+
+        }else{
+            if (numero<0) {
+                System.out.println("El numero" + numero + "Es NEGATIVO");
+            }else {
+
+            }
+
+
+
+        }
+            System.out.println();
+
+}
+    }
