db1118b70e1cfdf050aff14bc5e82856b70f9396
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-02 11:46:31 +0200

    Ejercicio 4 hecho en casa

---
+++ Ejercicios programacion/src/main/java/org/example/Ejercicio4.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println(" Escribe un numero ");
+        int num1 = teclado.nextInt();
+
+        System.out.println(" Escribe otro numero ");
+        int num2 = teclado.nextInt();
+
+        System.out.println( " Escribe otro numero ");
+        int num3 = teclado.nextInt();
+
+        if (num1 == num2 || num1 == num3 || num2 == num3) {
+            System.out.println("ERROR hay numeros introducidos que son iguales ");
+        }else {
+            if (num1 < num2 && num1 < num3) {
+                System.out.println("El numero" + num1 + " es el menor");
+
+                } else if (num2 < num1 && num2 < num3){
+                    System.out.println("El numero" + num2 + " es el menor");
+
+                }else {
+                System.out.println(" El numero " + num3 + " es el menor ");
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
