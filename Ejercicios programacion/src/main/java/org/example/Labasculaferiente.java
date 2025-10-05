1209badcb1ebe7601bddaa9a85fc59a6f34c46fa
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-05 19:29:26 +0200

    La bascula del feriante pero con las anotaciones

--- Ejercicios programacion/src/main/java/org/example/Labasculaferiente.java
+++ Ejercicios programacion/src/main/java/org/example/Labasculaferiente.java
@@ -19,9 +19,9 @@ public class Labasculaferiente {
 
        do {
            try {
-                                                //Pedimos la altura
+                                                //PEDIMOS LA ALTURA
                System.out.println(" INTRODUCE TU ALTURA EN CENTIMETROS PORFAVOR ");
-               altura = teclado.nextInt();
+               altura = teclado.nextInt();      //LA INTRODUCIMOS
 
                System.out.println("LEYENDO ALTURA (CM)... ");
                System.out.println(" MIDES: " + altura + "cm");
@@ -32,35 +32,35 @@ public class Labasculaferiente {
                System.out.println( " ERROR: EL FORMATO INTRODUCIDO NO ES EL CORRECTO... ");
                repetir_a = true;
            }
-
+                //SE REPETIRA EN CASO DE QUE EL BOOLEANO SEA TRUE
        }while (repetir_a == true);
-
+                    //SI ESTO NO SE CUMPLE EL PROGRAMA IMPRIMIRA LO SIGUIENTE Y HARA EL CALCULO DE CUANTO LE FALTA PARA LLEGAR
        if (altura > altura_maxima || altura < altura_minima){
            System.out.println(" LO SIENTO, NO LLEGAS A LA ALTURA PERMITIDA POR " + (altura - altura_maxima) + " CM ");
        }else{
 
            do {
-               try {
+               try {            //PEDIMOS EL PESO
                    System.out.println( " INTRODUCE TU PESO EN (KG) ");
                    peso = teclado.nextInt();
                    repetir_p = false;
-
+                                    //EN EL CASO DE QUE NOS INTRODUZCA UN FORMATO INCORRECTO VOLVERA A DAR ERROR
                }catch (Exception error){
                    System.out.println( " EL FORMATO INTRODUCIDO NO ES CORRECTO ");
                    repetir_p = true;
                }
 
            }while (repetir_p == true);
-
+                            //SI EXCEDE EL PESO MAXIMO EL PROGRAMA IMPRIMIRA LO SIGUENTE
            if (peso > peso_maximo){
                System.out.println( " LO SIENTO EXCEDES EL PESO PERMITIDO POR " + (peso - peso_maximo) + " KG ");
            }else {
-
+                            //SI NO SE CUMPLE TAMPOCO PODRA CONTINUAR
                if (peso < altura * 2 / 8) {
                    System.out.println(" LO SIENTO PESAS DEMASIADO POCO PARA TU ALTURA");
                } else {
 
-               }
+               }            //SI TODO LO ANTERIOR ESTA CORRECTO EL PROGRAMA TE DEJARA CONTINUAR ADELANTE 
                    System.out.println( " ENHORABUENA, PUEDES SUBIR!! ADELANTE!!");
            }
        }
