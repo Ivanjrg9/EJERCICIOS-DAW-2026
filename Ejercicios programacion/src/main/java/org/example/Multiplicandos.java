4d771c051c9198f4436a48ffed164ed9ea36293e
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-14 15:47:48 +0200

    Practica de las multiplicaciones

--- Ejercicios programacion/src/main/java/org/example/Multiplicandos.java
+++ Ejercicios programacion/src/main/java/org/example/Multiplicandos.java
@@ -5,12 +5,77 @@ import java.util.Scanner;
 public class Multiplicandos {
     static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        boolean repetir = false;
+        int multiplicando = 0;
+        int multiplicador = 0;
 
-        System.out.println( " Introduce el multiplicando (3 cifras): ");
-        int multiplicando = teclado.nextInt();
+        do {
+            repetir = false;
+            try {
+                System.out.println( " Introduce el Multiplicando (3 cifras): ");
+                multiplicando = teclado.nextInt();
 
-        System.out.println( " Introduce el multiplicador (3 cifras): ");
-        int multiplicador = teclado.nextInt();
+                System.out.println( " Introduce el Multiplicador (3 cifras): ");
+                multiplicador = teclado.nextInt();
+
+                if (multiplicando > 999 || multiplicando < 100 || multiplicando < 0 || multiplicador > 999 || multiplicador < 100 || multiplicador < 0){
+                    System.out.println( " ERROR: Los numeros deben de ser positivos y de exactamente 3 cifras ");
+                    repetir = true;
+                }
+
+            }catch (Exception error){
+                System.out.println(" ERROR: Introduce un formato correcto");
+                teclado.nextLine();
+                repetir = true;
+            }
+
+
+        }while (repetir);
+
+        // Convertir a string
+        String multiplicanding = Integer.toString(multiplicando);
+        String multiplicadoring = Integer.toString(multiplicador);
+
+        // Sacar las cifras del multiplicador
+        int cifra1 = Integer.parseInt(multiplicadoring.substring(0,1));
+        int cifra2 = Integer.parseInt(multiplicadoring.substring(1,2));
+        int cifra3 = Integer.parseInt(multiplicadoring.substring(2,3));
+
+        // Productos parciales
+        int parcial1 = multiplicando * cifra1;
+        int parcial2 = multiplicando * cifra2;
+        int parcial3 = multiplicando * cifra3;
+
+        // Total
+        int total = multiplicando * multiplicador;
+
+        // Mostrar los resultados
+
+        System.out.println();
+        System.out.println("  " + multiplicando);
+        System.out.println(" x " + multiplicador);
+        System.out.println("_______");
+        System.out.println("   " + parcial1);
+        System.out.println("  " + parcial2);
+        System.out.println(" " + parcial3);
+        System.out.println("______");
+        System.out.println(" " + total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
