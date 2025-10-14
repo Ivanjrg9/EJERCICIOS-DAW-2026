9f2744883abb46b77ce577a6ac50061c414a27a6
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-14 14:12:54 +0200

    correcciones

---
+++ Ejercicios programacion/src/main/java/org/example/Multiplicandos.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Multiplicandos {
+    static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println( " Introduce el multiplicando (3 cifras): ");
+        int multiplicando = teclado.nextInt();
+
+        System.out.println( " Introduce el multiplicador (3 cifras): ");
+        int multiplicador = teclado.nextInt();
+
+
+}
+    }
+
+
+
