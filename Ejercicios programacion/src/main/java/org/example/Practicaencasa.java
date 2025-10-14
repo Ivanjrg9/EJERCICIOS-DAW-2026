c4d622950d81fa2bd2cc84961141dd3eb6109ad7
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-14 12:40:13 +0200

    casa

--- Ejercicios programacion/src/main/java/org/example/Practicaencasa.java
+++ Ejercicios programacion/src/main/java/org/example/Practicaencasa.java
@@ -1,31 +1,34 @@
 package org.example;
 
 import java.util.Scanner;
-
 public class Practicaencasa {
     static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
-       System.out.println( "Vamos a realizar una sencilla division ");
-       System.out.println( " introduce el dividendo ");
 
-       int num1 = teclado.nextInt();
+        boolean repetir = true;
+        int suma = 0;
+        int num = 0;
 
-       System.out.println( " introduce el divisor ");
+        System.out.println( " Sumaremos numeros hasta que introduzcas el numero 0 ");
 
-       try {
-           int num2 = teclado.nextInt();
+        do {
+            try {
+                System.out.print("Introduce un número: ");
+                num = teclado.nextInt();
+                if (num > 0) {
+                    suma += num;
+                } else if (num < 0) {
+                    System.out.println("Número negativo ignorado.");
+                } else if (num == 0) {
+                    repetir = false; // cuando es 0, salimos
+                }
 
-           int resultado = num1 / num2;
-
-           System.out.println(" El resultado es " + resultado);
-
-       }catch (Exception error){
-
-         System.out.println( " Error: No se puede dividir entre 0 ");
-
-       }
-
-    }
+            } catch (Exception error) {
+                System.out.println("Error: entrada no válida.");
+                teclado.nextLine(); // limpiar el buffer
+            }
+        } while (repetir);
+        System.out.println("La suma total es: " + suma);
 
 
-}
+
