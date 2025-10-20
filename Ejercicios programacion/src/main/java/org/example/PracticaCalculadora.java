4eecadad54a93a4040a26e4d6f3a5de82acb53b4
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-10-20 21:19:28 +0200

    Practica calculadora

---
+++ Ejercicios programacion/src/main/java/org/example/PracticaCalculadora.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Scanner;
+
+public class PracticaCalculadora {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("---BIENVENID@---");
+        System.out.println(" ELIGE LA OPERACIÓN QUE VAS A REALIZAR ");
+        System.out.println(" 1.(+) 2.(-) 3.(/) 4.(R) ");
+
+    }
+}
