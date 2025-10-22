4bbdf941716ad1b40d2efe7ab0439abe00e6f5a2
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-22 14:08:30 +0200

    puliendo la calculadora

--- Ejercicios programacion/src/main/java/org/example/PracticaCalculadora.java
+++ Ejercicios programacion/src/main/java/org/example/PracticaCalculadora.java
@@ -5,57 +5,80 @@ import java.util.Scanner;
 public class PracticaCalculadora {
     static void main() {
         Scanner teclado = new Scanner(System.in);
+        double num = 0;
+        double num2 = 0;
 
 
         System.out.println("---BIENVENID@---");
 
         System.out.println(" Introduce un número ");
-        double num = teclado.nextDouble();
+        if (teclado.hasNextDouble()){
+             num = teclado.nextDouble();
+            System.out.println(" Introduce otro número ");
+            if (teclado.hasNextDouble()){
+                num2 = teclado.nextDouble();
+            }else {
+                System.out.println(" Formato incorrecto para el segundo número... ");
+                return;
+            }
 
-        System.out.println(" Introduce otro número ");
-        double num2 = teclado.nextDouble();
+        }else {
+            System.out.println(" Formato incorrecto para el primer número... ");
+            return;
+        }
 
         System.out.println(" ELIGE LA OPERACIÓN QUE VAS A REALIZAR ");
         System.out.println(" 1.(+) 2.(-) 3.(/) 4.(*) 5.(R) ");
 
 
-
         String operacion = teclado.next();
-        switch (operacion){
+        String formato_mayus = operacion.toUpperCase();
+        switch (formato_mayus){
 
             case "+":
                 System.out.println(" Has elegido sumar ");
 
                 double resultado = num + num2;
-                System.out.println("El resultado de la suma es: " + resultado);
+                System.out.println(" El resultado de " + num + " + " + num2 + " es: " + resultado);
+
                 break;
 
             case "-":
                 System.out.println(" Has elegido restar ");
 
                 double resultado_resta = num - num2;
-                System.out.println(" El resultado de la resta es:" + resultado_resta);
+                System.out.println(" El resultado de " + num + " - " + num2 + " es: " + resultado_resta);
                 break;
 
             case "/":
                 System.out.println(" Has elegido dividir ");
-                try {
-                    double resultado_division = num / num2;
-                    System.out.println(" El resultado de la divión es: " + resultado_division);
 
-                }catch (Exception error){
-                    System.out.println(" ERROR: Formato de la división incorrecto... ");
-                }
+                    if ( num2 == 0 ){
+                        System.out.println(" ERROR: Formato de la división incorrecto, divides entre 0... ");
+                    }else {
+                        double resultado_division = num / num2;
+                        System.out.println(" El resultado de " + num + " / " + num2 + " es: " + resultado_division);
+                    }
                 break;
 
             case "*":
                 System.out.println(" Has elegido multiplicar ");
                 double resultado_multiplicacion = num * num2;
-                System.out.println( " El resultado de la multiplicación es: " + resultado_multiplicacion);
+                System.out.println(" El resultado de " + num + " * " + num2 + " es: " + resultado_multiplicacion);
                 break;
 
             case "R":
-                
+                System.out.println( " Has elegido la Raíz cuadrada ");
+                if ( num < 0 ){
+                    System.out.println( " ERROR: No se puede ejecutar la raíz cuadrada de numeros negativos ");
+
+                }else {
+                   double resultado_raiz = Math.sqrt(num);
+                    System.out.println(" El resultado de la Raíz cuadrada de " + num + " es: " + resultado_raiz);
+
+
+                }
+
 
 
 
