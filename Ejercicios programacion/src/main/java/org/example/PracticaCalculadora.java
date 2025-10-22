deccec94f7f63e040148040f3aa6dce63eb2339f
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-10-22 14:42:28 +0200

    calculadora arreglando la raiz

--- Ejercicios programacion/src/main/java/org/example/PracticaCalculadora.java
+++ Ejercicios programacion/src/main/java/org/example/PracticaCalculadora.java
@@ -7,21 +7,12 @@ public class PracticaCalculadora {
         Scanner teclado = new Scanner(System.in);
         double num = 0;
         double num2 = 0;
-
+        
 
         System.out.println("---BIENVENID@---");
-
         System.out.println(" Introduce un número ");
         if (teclado.hasNextDouble()){
-             num = teclado.nextDouble();
-            System.out.println(" Introduce otro número ");
-            if (teclado.hasNextDouble()){
-                num2 = teclado.nextDouble();
-            }else {
-                System.out.println(" Formato incorrecto para el segundo número... ");
-                return;
-            }
-
+            num = teclado.nextDouble();
         }else {
             System.out.println(" Formato incorrecto para el primer número... ");
             return;
@@ -30,9 +21,37 @@ public class PracticaCalculadora {
         System.out.println(" ELIGE LA OPERACIÓN QUE VAS A REALIZAR ");
         System.out.println(" 1.(+) 2.(-) 3.(/) 4.(*) 5.(R) ");
 
-
         String operacion = teclado.next();
         String formato_mayus = operacion.toUpperCase();
+        if ( formato_mayus == "R" ){
+            System.out.println( " Has elegido la Raíz cuadrada ");
+            if ( num < 0 ){
+                System.out.println( " ERROR: No se puede ejecutar la raíz cuadrada de numeros negativos ");
+
+            }else {
+                double resultado_raiz = Math.sqrt(num);
+                System.out.println(" El resultado de la Raíz cuadrada de " + num + " es: " + resultado_raiz);
+
+            }
+
+        }else {
+            System.out.println(" Introduce otro número ");
+            if (teclado.hasNextDouble()){
+                num2 = teclado.nextDouble();
+            }else {
+                System.out.println(" Formato incorrecto para el segundo número... ");
+                return;
+            }
+
+        }
+
+        if (formato_mayus.length()== 1){
+
+        }else {
+            System.out.println(" ERROR: Introduce solo un operando porfavor... ");
+        }
+
+
         switch (formato_mayus){
 
             case "+":
@@ -67,17 +86,9 @@ public class PracticaCalculadora {
                 System.out.println(" El resultado de " + num + " * " + num2 + " es: " + resultado_multiplicacion);
                 break;
 
-            case "R":
-                System.out.println( " Has elegido la Raíz cuadrada ");
-                if ( num < 0 ){
-                    System.out.println( " ERROR: No se puede ejecutar la raíz cuadrada de numeros negativos ");
+            default:
+                System.out.println(" Elige una opcion correcta porfavor... ");
 
-                }else {
-                   double resultado_raiz = Math.sqrt(num);
-                    System.out.println(" El resultado de la Raíz cuadrada de " + num + " es: " + resultado_raiz);
-
-
-                }
 
 
 
