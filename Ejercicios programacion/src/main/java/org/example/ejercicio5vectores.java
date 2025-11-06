adc838ef74e771fcaf4c4f56c880e0a1f187fefe
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-11-06 17:07:28 +0100

    4-5-6 ejercicios clase

--- Ejercicios programacion/src/main/java/org/example/ejercicio5vectores.java
+++ Ejercicios programacion/src/main/java/org/example/ejercicio5vectores.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,6 +8,19 @@ public class ejercicio5vectores {
     static void main() {
         Scanner teclado = new Scanner(System.in);
         Random aleatorio= new Random();
+
+        int vectotr[]= {4,5,7,0,3,9};
+
+        int aux = vectotr[0];
+
+        for (int i = 0; i < vectotr.length - 1 ; i++) {
+            vectotr[i]=vectotr[i+1];
+
+        }
+
+        vectotr[vectotr.length-1]=aux;
+        System.out.println(Arrays.toString(vectotr));
+
 
 
     }
---
+++ Ejercicios programacion/src/main/java/org/example/ejercicio6vectores.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ejercicio6vectores {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        int vector[] = {1,2,3,2,1};
+
+        for (int i = 0; i < vector.length; i++) {
+
+            if (vector[i]!=vector[vector.length - 1 - i]){
+                System.out.println("No es");
+                break;
+
+            }
+        }
+        System.out.println("Es palindromo");
+    }
+}
