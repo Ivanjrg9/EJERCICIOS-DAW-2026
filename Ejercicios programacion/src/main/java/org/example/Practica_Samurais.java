0cce0a6d8a0f365efba3404ec0717c29883564ef
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-11-07 19:15:02 +0100

    continuacion samurais

--- Ejercicios programacion/src/main/java/org/example/Practica_Samurais.java
+++ Ejercicios programacion/src/main/java/org/example/Practica_Samurais.java
@@ -10,7 +10,7 @@ public class Practica_Samurais {
         Random aleatorio = new Random();
 
         final int POTENCIA_MAX=30;
-
+        int suma =0;
 
 
         System.out.println("BIENVENID@ A LA BATALLA DE SAMURAIS");
@@ -18,16 +18,40 @@ public class Practica_Samurais {
         System.out.println("EQUIPO 1");
         System.out.println("INTRODUCE LA POTENCIA DE TUS 7 SAMURAIS (POTENCIA MÁX 24 Y SEPARADOS POR ESPACIOS...):");
         String potencia= teclado.nextLine();
+        String vpotencia[] = potencia.split(" ");
 
-
-        if (potencia.length()==13){
-            String vpotencia[] = potencia.split(" ");
+        if (vpotencia.length ==7){
             System.out.println(Arrays.toString(vpotencia));
+
+            for (int i = 0; i < vpotencia.length; i++) {
+                suma += Integer.parseInt(vpotencia[i]);
+            }
+
         }else {
             System.out.println("ERROR: INTRODUCE SOLO 7 SAMURAIS...");
-
         }
+        if (suma>POTENCIA_MAX){
+            System.out.println("ERROR: LA SUMA TOTAL NO SUMA 30...");
+
+        }else {
+            System.out.println("Equipo 1 completo!");
+        }
+        System.out.println("----------------------------------");
+
+        System.out.println("EQUIPO 2, TU TURNO...");
+        System.out.println("INTRODUCE LA POTENCIA DE TUS 7 SAMURAIS (POTENCIA MÁX 24 Y SEPARADOS POR ESPACIOS...):");
+        String potencia2 = teclado.nextLine();
+        String vpotencia2[]= potencia2.split(" ");
+
         
+
+
+
+
+
+
+
+
 
 
 
