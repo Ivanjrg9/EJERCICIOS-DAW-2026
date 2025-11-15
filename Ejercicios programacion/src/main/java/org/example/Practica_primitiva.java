d3cc56757d818012aa8329305ecd9da86fbccd99
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-11-15 22:12:31 +0100

    Entrada de la practica de la primitiva

--- Ejercicios programacion/src/main/java/org/example/Practica_primitiva.java
+++ Ejercicios programacion/src/main/java/org/example/Practica_primitiva.java
@@ -5,32 +5,34 @@ import java.util.Scanner;
 
 public class Practica_primitiva {
     static void main() {
-        Scanner teclado= new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         Random aleatorio = new Random();
 
+        boolean correcto=true;
+        String numeros_boleto;
 
 
         do {
             System.out.println("INTRODUCE LOS NÚMEROS DE TU BOLETO (DEL 0 AL 49 Y N-N-N-N-N-N/R)");
-            String numeros_boleto= teclado.nextLine();
-            boolean formato = numeros_boleto.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1}");
+            numeros_boleto=teclado.nextLine();
+            String num_voleto_array[] = numeros_boleto.split("-");
 
-        }while ()
+            for (int i = 0; i < num_voleto_array.length; i++) {
+                if (i>49 || i<0){
+                    System.out.println("ERROR INTRODUCE NUMEROS ENTRE 1 Y 49");
+                    correcto=false;
+                }
+            }
 
+            if (numeros_boleto.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1}")){
+                correcto=true;
 
+            }else {
+                System.out.println("FORMATO INCORRECTO");
+                correcto=false;
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        }while (correcto!=true);
 
 
 
@@ -42,4 +44,5 @@ public class Practica_primitiva {
 
 
     }
+
 }
