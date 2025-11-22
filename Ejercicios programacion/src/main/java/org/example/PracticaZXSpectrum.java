4270c2a0619831a57890a8977e821c34cfa7f3ca
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-11-23 00:56:51 +0100

    bucle practica jueguito

--- Ejercicios programacion/src/main/java/org/example/PracticaZXSpectrum.java
+++ Ejercicios programacion/src/main/java/org/example/PracticaZXSpectrum.java
@@ -6,19 +6,23 @@ public class PracticaZXSpectrum {
     static void main() {
         Scanner teclado = new Scanner(System.in);
 
+        String letraspixel;
+
+
         System.out.println("**************************************************************************");
-        System.out.println("                 VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM");
+        System.out.println("                >VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM<");
         System.out.println("**************************************************************************");
 
         System.out.println("Introduce la resolución de tu pantalla (Alto x Ancho): ");
         System.out.println(">(Ambos deben ser múltiplos de 8 y no mayores de 48)<");
         System.out.println("--------------------------------------------------------------------------");
 
-        System.out.println("Alto: ");
-        int alto = teclado.nextInt();
-
         System.out.println("Ancho: ");
         int ancho = teclado.nextInt();
+
+        System.out.println("Alto: ");
+        int alto = teclado.nextInt();
+        teclado.nextLine();
 
         if (alto % 8 == 0 && ancho % 8 == 0 && alto < 48 && ancho < 48){
             System.out.println("----------------------------------------------------------------------");
@@ -26,9 +30,22 @@ public class PracticaZXSpectrum {
             System.out.println("----------------------------------------------------------------------");
             System.out.println("Introduce (línea a línea) los colores de tu imagen para cada píxel: ");
 
+            for (int i = 0; i < alto; i++) {
+                 letraspixel = teclado.nextLine();
+            }
+
+            int matriz[][] = new int[alto][ancho];
+
         }else {
             System.out.println("Error: Introduce solo múltiplos de 8 y que no sean mayores de 48...");
         }
+
+
+
+
+
+
+
 
 
 
