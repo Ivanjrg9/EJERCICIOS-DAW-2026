0319510e6818ba6568a307c150ec4605041d58a8
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2025-11-25 12:59:18 +0100

    codigo comentado

--- Ejercicios programacion/src/main/java/org/example/PracticaZXSpectrum.java
+++ Ejercicios programacion/src/main/java/org/example/PracticaZXSpectrum.java
@@ -35,46 +35,54 @@ public class PracticaZXSpectrum {
 
         }
 
-        if (alto % 8 == 0 && ancho % 8 == 0 && alto < 48 && ancho < 48){
+        if (alto % 8 == 0 && ancho % 8 == 0 && alto < 48 && ancho < 48){//Creamos el filtro para que sean multiplos y no mayores de 48 los datos de la resolucion
             System.out.println("----------------------------------------------------------------------");
             System.out.println("                            Correcto!");
             System.out.println("----------------------------------------------------------------------");
             System.out.println("Introduce (línea a línea) los colores de tu imagen para cada píxel: ");
 
-            String matriz[][] = new String[alto][ancho];
+            String matriz[][] = new String[alto][ancho];//Creamos matriz
 
             for (int i = 0; i < alto; i++) {
-                String filapixel= teclado.nextLine();
+                String filapixel= teclado.nextLine();//Pedimos las filas de letras
 
-                if (filapixel.length() != ancho) {
+                if (filapixel.length() != ancho) {//Comprobamos que coincida exactamente con el numero de ancho que hayamos puesto
                     System.out.println("ERROR: La línea debe tener exactamente " + ancho + " letras.");
-                    i--;
+                    i--;//Si salta el error la i vuelve 1 para atras para que no cuente el error como dato valido
                     continue;
                 }
-                if (!filapixel.matches("[a-oA-O]+")) {
+                if (!filapixel.matches("[a-oA-O]+")) {//Nos aseguramos de que no introduzca ninguna letra que no entre en el rango introducido
                     System.out.println("ERROR: Solo puedes usar letras de la A a la O.");
-                    i--;
+                    i--;//Igual que antes, si hay error la i vuelve atras y no cuenta
                     continue;
                 }
-                columnapixel = filapixel.split("");
+                columnapixel = filapixel.split("");//pasamos a vector los datos introducidos para asi poder utilizarlos en una matriz
 
-                for (int j = 0; j < ancho; j++) {
+                for (int j = 0; j < ancho; j++) { //rellenamos la matriz
                     matriz[i][j] = columnapixel[j];
                 }
 
             }
-            for (String filas[]: matriz){
+            for (String filas[]: matriz){ //printeamos la matriz
                 System.out.println(Arrays.toString(filas));
             }
 
-            int contador=0;
+            int contador=0; //Y aqui me quedo, he intentado esto que ves a continuacion pero sin exito, antes que usar el chat gpt prefiero entregarte esto... lo siento :(
 
             for (int i = 0; i < matriz.length; i+=8) {
 
                 for (int j = 0; j < matriz[i].length; j+=8) {
+                    if (matriz[i]==matriz[j]){
+                        contador++;
+                    }
 
                 }
 
+            }
+            if (contador>3){
+                System.out.println("No es compatible con ZXSpectrum");
+            }else {
+                System.out.println("Es compatible con ZXSpectrum");
             }
 
         }else {
