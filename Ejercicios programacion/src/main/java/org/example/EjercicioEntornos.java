8b460d9452a615fddf89c5119500f41ee9f7c7f0
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-12-03 19:42:11 +0100

    ejercicios de entornos de desarollo

---
+++ Ejercicios programacion/src/main/java/org/example/EjercicioEntornos.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class EjercicioEntornos {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Cuantas personas no conoce Lotario?");
+        int personas = teclado.nextInt();
+
+        System.out.println("Como se llaman?");
+        for (int i = 0; i < personas; i++) {
+            String nombres = teclado.next();
+            System.out.println("---------------------");
+            System.out.println("Hola " + nombres + " ;)");
+            System.out.println("---------------------");
+        }
+    }
+}
