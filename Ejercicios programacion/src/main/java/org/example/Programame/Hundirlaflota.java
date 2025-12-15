87c820a5a387233b8ced5bb14329b1c446b7c8b0
A612\1DAW <ramosgamarraivanjose@yahoo.es>
2025-12-15 21:07:42 +0100

    continuacion de hundir la flota

--- Ejercicios programacion/src/main/java/org/example/Programame/Hundirlaflota.java
+++ Ejercicios programacion/src/main/java/org/example/Programame/Hundirlaflota.java
@@ -1,5 +1,6 @@
 package org.example.Programame;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hundirlaflota {
@@ -21,11 +22,12 @@ public class Hundirlaflota {
         if (numerobarcos>0 && numerobarcos<=10){
             System.out.println("---------------------------------------------------");
             System.out.println("Introduce el tamaño para cada uno (Mín 2, Máx 5):");
+            int barcos[] = new int [numerobarcos];
 
             for (int i = 0; i < numerobarcos; i++) {
                 System.out.print("Barco " + (i+1) + " :");
-                int tamanyobarcos = teclado.nextInt();
-                if (tamanyobarcos < 2 || tamanyobarcos > 5){
+                    barcos[i] = teclado.nextInt();
+                if (barcos[i] < 2 || barcos[i] > 5){
                     System.out.println("Introdúcelo de nuevo, tamaño incorrecto...");
                     i--;
                     continue;
@@ -38,8 +40,24 @@ public class Hundirlaflota {
     }
 
     public static void tablero(){
-        System.out.println("Ahora introduce tu tablero: ");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("----------------------------------------------------");
+        System.out.println("Ahora introduce las dimensiones de tu tablero máximo 128x128:");
 
+        System.out.println("Dimensiones:");
+        int dimensiones = teclado.nextInt();
 
+        System.out.println("Relleta tu tablero");
+        int tablero[][]=new int[dimensiones][dimensiones];
+
+        for (int i = 0; i < dimensiones; i++) {
+            for (int j = 0; j < dimensiones; j++) {
+                tablero[i][j]= teclado.nextInt();
+            }
+        }
+
+        for (int filas[]:tablero){
+            System.out.println("Tu tablero " + Arrays.toString(filas));
+        }
     }
 }
---
+++ Ejercicios programacion/src/test/java/org/example/Programame/HundirlaflotaTest.java
@@ -0,0 +1,11 @@
+package org.example.Programame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HundirlaflotaTest {
+
+    @org.junit.jupiter.api.Test
+    void barcos() {
+
+    }
+}
